f310a6333fb86ae47690f964c7f433ed52fd1a00
lilnounsDAO <officialericdevito@yahoo.com>
2024-06-11 02:35:32 +0100

    embed fomonouns

---
+++ components/FomoNounsEmbed.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FomoNounsEmbed = () => {
+  return (
+    <div className="h-96 w-full lg:h-full">
+      <iframe src="https://fomonouns.wtf" className="h-full w-full border-none" title="Embedded Page" />
+    </div>
+  )
+}
+
+export default FomoNounsEmbed
